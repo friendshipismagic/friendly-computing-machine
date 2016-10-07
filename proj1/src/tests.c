3b19158e9d7ed171c1ceaf49387dddde2d81e97e
Alexandre JANNIAUX <alexandre.janniaux@gmail.com>
2016-10-07 14:48:14 +0200

    Add unit test for full_adder.

--- proj1/src/tests.c
+++ proj1/src/tests.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 
 void pass() {
-    printf("\t\t[OK]\n");
+    printf("\t\t\t\t[OK]\n");
 }
 
 void test(char* str) {
@@ -25,12 +25,29 @@ void half_adder_test(char a, char b, char s, char c) {
     pass();
 }
 
+void full_adder_test(char a, char b, char c_in, char s, char c) {
+    char _s = 0, _c = 0;
+    char msg[40];
+    sprintf(msg, "full_adder a:%c b:%c c_in:%c", a, b, c_in);
+    test(msg);
+    full_adder(a,b,c_in, &_s, &_c);
+    assert(s==_s && c==_c);
+    pass();
+}
+
 int main() {
     half_adder_test('0','0','0','0');
     half_adder_test('0','1','1','0');
     half_adder_test('1','0','1','0');
     half_adder_test('1','1','0','1');
 
-
+    full_adder_test('0','0','0','0','0');
+    full_adder_test('1','0','0','1','0');
+    full_adder_test('0','1','0','1','0');
+    full_adder_test('0','0','1','1','0');
+    full_adder_test('1','1','0','0','1');
+    full_adder_test('1','0','1','0','1');
+    full_adder_test('0','1','1','0','1');
+    full_adder_test('1','1','1','1','1');
     return 0;
 }
