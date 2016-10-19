ad3aba600aa08659dc125522ae5c4e3aea8c696a
Tuetuopay <tuetuopay@me.com>
2016-10-19 11:33:49 +0200

    Structure of the compiler

---
+++ proj1/src/compiler/compile.js
@@ -0,0 +1,17 @@
+"use strict";
+
+	/**
+	 * Friendly compiler for the friendly computing machine
+	 */
+
+// This structure is used to avoid polluting the global namespace
+var compileAsm = (function(){
+	// Instruction list
+	var instructions = {
+	};
+
+	return function(src) {
+	};
+})();
+
+
