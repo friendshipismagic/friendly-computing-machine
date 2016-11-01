0c54e9a89936e23b31494b2bb39ac21ee38ebf4c
Tuetuopay <tuetuopay@me.com>
2016-11-01 23:07:52 +0100

    Fixed the ASM samples

--- proj1/src/c_code.s
+++ proj1/src/c_code.s
@@ -2,11 +2,10 @@ main:
 	lwi   $r1 200h ; a
 	lwi   $r2 0h   ; s
 	lwi   $r3 ah   ; i
-	lwi   $r5 4h   ; constant 4
-	lwi   $r6 1h   ; constant 1
+	lwi   $r5 1h   ; constant 1
 	loop:
-	lw   $r1 $r4 0h  ; r4 = *(a + 0)
-	add  $r2 $r2 $r4 ; s = s + r4 <=> s += *a
-	add  $r1 $r1 $r5 ; a += 4
-	sub  $r3 $r3 $r6 ; i++
-	bne  -4h $r3 $r0
+	lw    $r1 $r4 0h  ; r4 = *(a + 0)
+	add   $r2 $r2 $r4 ; s = s + r4 <=> s += *a
+	addiu $r1 4h      ; a += 4
+	sub   $r3 $r3 $r5 ; i++
+	bne   -4h $r3 $r0
--- proj1/src/two_pow_21.s
+++ proj1/src/two_pow_21.s
@@ -1,8 +1,9 @@
 main:
 	lwi $r1 2h
-	lwi $r2 14h ; load 20
-	lwi $r3 1h
+	lwi $r2 2h
+	lwi $r3 14h ; load 20
+	lwi $r4 1h
 	loop:
 	mulu  $r1 $r1 $r2
-	sub   $r2 $r2 $r3
-	bne   loop $r2 $r0  ; loop
+	sub   $r3 $r2 $r3
+	bne   loop $r3 $r0  ; loop
